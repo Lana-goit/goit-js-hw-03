e8450e340d87a5de96969546ccb121634260fff5
Lana Petrunok <162810451+Lana-goit@users.noreply.github.com>
2024-05-31 23:58:54 +0300

    add index.js file

--- js/task-3.js
+++ js/task-3.js
@@ -6,3 +6,18 @@
 // Використай цикл для ітерації кожного елемента масиву numbers.
 // Використовуй умовний оператор if усередині циклу для перевірки кожного елемента и додавання до свого масиву.
 // Поверни свій новий масив з підходящими числами як результат.
+
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  for (const num of numbers) {
+    if (num > value) {
+      filteredNumbers.push(num);
+    }
+  }
+  return filteredNumbers;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
